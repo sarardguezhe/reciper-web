fa30c0f8d2a9591e4be2cbc4515c775c3b44a35e
sarardguezhe <sara.rodriguezhe@gmail.com>
2026-03-11 00:36:07 +0100

    fix: deploy timeout database

--- Back/src/utils/database.js
+++ Back/src/utils/database.js
@@ -1,26 +1,49 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
+const mongoose = require("mongoose");
 
-const DB_URL= process.env.DB_URI;
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null };
+}
 
 const connect = async () => {
+  if (cached.conn) return cached.conn;
 
-  try {
+  cached.conn = await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
 
-    const db = await mongoose.connect(DB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    const { name, host } = db.connection;
-
-    console.log(`Connect to: ${name} DB. Host: ${host}`);
-
-  } catch (error) {
-
-    console.log(`Error connecting the DB: ${error}`);
-
-  }
+  return cached.conn;
 };
 
 module.exports = { connect };
+
+
+
+// const mongoose = require('mongoose');
+// const dotenv = require('dotenv').config();
+
+// const DB_URL= process.env.DB_URI;
+
+// const connect = async () => {
+
+//   try {
+
+//     const db = await mongoose.connect(DB_URL, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+
+//     const { name, host } = db.connection;
+
+//     console.log(`Connect to: ${name} DB. Host: ${host}`);
+
+//   } catch (error) {
+
+//     console.log(`Error connecting the DB: ${error}`);
+
+//   }
+// };
+
+// module.exports = { connect };
