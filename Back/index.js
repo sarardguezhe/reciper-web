7bb0cc5ededc3beb2fe18ea68dbe07631c2d4717
sarardguezhe <sara.rodriguezhe@gmail.com>
2026-03-11 00:05:45 +0100

    feat: vercel config

--- Back/index.js
+++ Back/index.js
@@ -1,44 +1,57 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const { connect } = require('./src/utils/database');
-const cors = require('cors');
-const cloudinary = require('cloudinary').v2;
-const recipeRouter = require('./src/api/routes/recipes.routes');
-const ingredientRouter = require('./src/api/routes/ingredients.routes');
-const userRouter = require('./src/api/routes/user.routes');
-const commentsRouter = require('./src/api/routes/comments.routes');
+//Solo para conexión local:
 
-
-dotenv.config();
-// cloudinary.config({
-//   cloud_name: process.env.CLOUDINARY_NAME,
-//   api_key: process.env.CLOUDINARY_KEY,
-//   api_secret: process.env.CLOUDINARY_SECRET,
-//   secure: true,
-// });
+const app = require('./app');
 
 const PORT = 5020 || 5001;
-const app = express();
-app.use(cors(
-  {
-    origin:"*",
-    credentials:true
-  }
-));
-app.use(express.json());
-
-app.use('/recipes', recipeRouter);
-app.use('/ingredients', ingredientRouter);
-app.use('/user', userRouter)
-app.use('/comments', commentsRouter)
-
-
-connect();
-
-
 
 app.listen(PORT, () => {
   console.log(`Listening http://localhost:${PORT}`);
 });
 
-//
+
+//VERSIÓN ANTERIOR SIN ARCHIVO APP.JS:
+
+// const express = require('express');
+// const dotenv = require('dotenv');
+// const { connect } = require('./src/utils/database');
+// const cors = require('cors');
+// const cloudinary = require('cloudinary').v2;
+// const recipeRouter = require('./src/api/routes/recipes.routes');
+// const ingredientRouter = require('./src/api/routes/ingredients.routes');
+// const userRouter = require('./src/api/routes/user.routes');
+// const commentsRouter = require('./src/api/routes/comments.routes');
+
+
+// dotenv.config();
+// // cloudinary.config({
+// //   cloud_name: process.env.CLOUDINARY_NAME,
+// //   api_key: process.env.CLOUDINARY_KEY,
+// //   api_secret: process.env.CLOUDINARY_SECRET,
+// //   secure: true,
+// // });
+
+// const PORT = 5020 || 5001;
+// const app = express();
+// app.use(cors(
+//   {
+//     origin:"*",
+//     credentials:true
+//   }
+// ));
+// app.use(express.json());
+
+// app.use('/recipes', recipeRouter);
+// app.use('/ingredients', ingredientRouter);
+// app.use('/user', userRouter)
+// app.use('/comments', commentsRouter)
+
+
+// connect();
+
+
+
+// app.listen(PORT, () => {
+//   console.log(`Listening http://localhost:${PORT}`);
+// });
+
+
