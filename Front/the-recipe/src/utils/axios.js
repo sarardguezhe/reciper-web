cb222e82f5659b1e711d5c5d900ff35399a944a4
sarardguezhe <sara.rodriguezhe@gmail.com>
2026-04-28 23:38:11 +0200

    refactor: restructure frontend + fix react-scripts

--- Front/the-recipe/src/utils/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const axiosInstance = axios.create({
-    baseURL : 'https://reciper-api.vercel.app',
-    headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${localStorage.getItem("token")}`,
-      }
-})
